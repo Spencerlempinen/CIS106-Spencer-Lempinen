414381067006814906dd512307339c84488380e8
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-01-24 12:13:07 -0600

    Create Activity 7.py

---
+++ Assignment 3/Activity 7.py
@@ -0,0 +1,6 @@
+# This program Converts a dogs age in "human years" to a dogs age in "dog years"
+print("what is your dogs name and age?")
+name = input()
+age = int(input())
+age = age * 7
+print(name + " is " + str(age) + " dog years old!")
