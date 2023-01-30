1254c4360dba0ee45bc420aca30464e4fed66946
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-01-30 11:25:17 -0600

    Update Activity 1.py

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -1,14 +1,14 @@
-#This program will take hours worked per week and rate of pay and calculate weekly,monthly, and annual pay
-#References:
-#https://www.youtube.com/watch?v=1BB1sCkS9oU
+# This program will calculate weekly, monthly, and annual pay
+# References:
+# https://www.youtube.com/watch?v=1BB1sCkS9oU
 
 print("Enter hours wokred per week:")
-Hours_worked_per_week=float(input())
+Hours_worked_per_week = float(input())
 print("Enter rate per hour")
-Rate_per_hour=float(input())
+Rate_per_hour = float(input())
 Weekly_pay=Hours_worked_per_week * Rate_per_hour
 Monthly_pay=Weekly_pay * 4
 Annual_pay=Monthly_pay * 12
-print("$" + str(Weekly_pay)+ " per week")
+print("$" + str(Weekly_pay) + " per week")
 print("$" + str(Monthly_pay) + " per month")
 print("$" + str(Annual_pay) + " annually")
