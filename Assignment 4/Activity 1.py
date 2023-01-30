a3baddd9a04617bac582197ae7c4b7274c95a8fc
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-01-30 11:29:44 -0600

    Update Activity 1.py

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -3,12 +3,17 @@
 # https://www.youtube.com/watch?v=1BB1sCkS9oU
 
 print("Enter hours wokred per week:")
+
 Hours_worked_per_week = float(input())
+
 print("Enter rate per hour")
+
 Rate_per_hour = float(input())
-Weekly_pay = Hours_worked_per_week * Rate_per_hour
-Monthly_pay = Weekly_pay * 4
-Annual_pay = Monthly_pay * 12
+
+Weekly_pay=Hours_worked_per_week * Rate_per_hour
+Monthly_pay=Weekly_pay * 4
+Annual_pay=Monthly_pay * 12
+
 print("$" + str(Weekly_pay) + " per week")
 print("$" + str(Monthly_pay) + " per month")
 print("$" + str(Annual_pay) + " annually")
