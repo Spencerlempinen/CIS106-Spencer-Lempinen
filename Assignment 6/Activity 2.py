0b77118f9c82cb5bf6cb838ef0c66bec66d5ab17
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-02-06 13:36:15 -0600

    Update Activity 2.py

--- Assignment 6/Activity 2.py
+++ Assignment 6/Activity 2.py
@@ -3,20 +3,24 @@ def calculate_age_months(years):
 
     return age_months
 
+
 def calculate_age_days(years):
     age_days = years * 365
 
     return age_days
+
 
 def calculate_age_hours(age_days):
     age_hours = age_days * 24
 
     return age_hours
 
+
 def calculate_age_seconds(age_hours):
     age_seconds = age_hours * 3600
 
     return age_seconds
+
 
 def get_age():
     print("Please enter your age in years: ")
@@ -24,11 +28,13 @@ def get_age():
 
     return years
 
+
 def display_results(age_months, age_days, age_hours, age_seconds):
-    print( str(age_months) + " Months old")
-    print( str(age_days) + " Days old")
-    print( str(age_hours) + " Hours old")
-    print( str(age_seconds) + " Seconds old")
+    print(str(age_months) + " Months old")
+    print(str(age_days) + " Days old")
+    print(str(age_hours) + " Hours old")
+    print(str(age_seconds) + " Seconds old")
+
 
 # Main
 # This program will calculate someones age in months, days, hours, seconds.
