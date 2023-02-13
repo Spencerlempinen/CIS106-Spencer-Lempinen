f11d4dd1e04522395d475e1739b8b3ea6c8a4505
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-02-13 11:53:16 -0600

    Update Activity 2.py

--- Assignment 7/Activity 2.py
+++ Assignment 7/Activity 2.py
@@ -1,50 +1,47 @@
 # This program will calculate someones age in months, days, hours, seconds.
-# References: https://www.youtube.com/watch?v=AWek49wXGzI
+#References: https://www.youtube.com/watch?v=AWek49wXGzI
 
 
 def calculate_months(years):
     age_months = years * 12
+
     return age_months
 
 
 def calculate_days(years):
     age_days = years * 365
+
     return age_days
 
 
 def calculate_hours(age_days):
     age_hours = age_days * 24
+
     return age_hours
 
 
 def calculate_seconds(age_hours):
     age_seconds = age_hours * 3600
+
     return age_seconds
 
 
 def get_age():
     print("Please enter your age in years: ")
     years = float(input())
+
     return years
 
-
 def get_unit():
-    print("Would you like your age in (M)onths, (D)ays, (H)ours, (S)econds?")
+    print("Would you like your age in (M)onths,(D)ays,(H)ours,(S)econds?")
     unit = str(input())
+    
     return unit
 
 
-def display_results(years, unit, age_months, age_days, age_hours, age_seconds):
-    if unit == "M":
-        print(str(age_months) + " Months Old")
-    elif unit == "D":
-        print(str(age_days) + " Days old")
-    elif unit == "H":
-        print(str(age_hours) + " Hours old")
-    elif unit == "S":
-        print(str(age_seconds) + " Seconds old")
-    else:
-        return None
+def display_results(results):
+    print(results)
+
     
     
 def main():
@@ -54,7 +51,18 @@ def main():
     age_days = calculate_days(years)
     age_hours = calculate_hours(age_days)
     age_seconds = calculate_seconds(age_hours)
-    display_results(years, unit, age_months, age_days, age_hours, age_seconds)
+
+    if unit == "M":
+        display_results(str(age_months) + " Months Old")
+    elif unit == "D":
+        display_results(str(age_days) + " Days old")
+    elif unit == "H":
+        display_results(str(age_hours) + " Hours old")
+    elif unit == "S":
+        display_results(str(age_seconds) + " Seconds old")
+    else:
+        display_results("Invalid input")
+        
 
 
 main()
