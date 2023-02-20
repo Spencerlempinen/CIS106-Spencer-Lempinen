6bf95395f7e5e2d6be248382841bfe0d0aea5659
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-02-20 10:12:31 -0600

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -11,19 +11,30 @@ def get_value():
 
 def get_amount():
     print("please eneter the number of expressions you would like: ")
-    amount = float(input())
+    amount = int(input())
     return amount
+
+
+def generate_expressions(amount, value):
+    expressions = []
+    for i in range(1, amount + 1):
+        result = value * i
+        expression = f"{value} * {i} = {result}"
+        expressions.append(expression)
+    return expressions
+
+
+def display_output(expressions):
+    print("here are the multiplication expressions: ")
+    for expression in expressions:
+        print(expression)    
 
 
 def main():
     value = get_value()
     amount = get_amount()
-   
-
-
-    for i in range(1,int(amount) + 1):
-        result = value * i
-        print(value, "*", i, "=", result)
+    expressions = generate_expressions(amount, value)
+    display_output(expressions)
 
 
 main()
