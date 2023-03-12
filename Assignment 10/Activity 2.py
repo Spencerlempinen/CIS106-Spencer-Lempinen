f8e9a6fd441845fa40f142e03553a9fcb9c55971
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-03-12 14:51:28 -0500

    Update Activity 2.py

--- Assignment 10/Activity 2.py
+++ Assignment 10/Activity 2.py
@@ -3,21 +3,26 @@
 # https://www.youtube.com/watch?v=M3zmcj8sNu8&ab_channel=GeekTutorials
 
 
+low = 0
+high = 100
+guesses = 0
+
+
 def ask_for_number():
-    return input("Think of a number between 1 and 100, then press Enter to continue...")
+    input("Think of a number between 1 and 100, then press Enter to continue...")
 
-
-def process_guess(low, high, guesses):
-    while True:
-        mid = (low + high) // 2
-        response = input("Is your number " + str(mid) + "? ([h]igh/[l]ow/[e]qual)")
-        guesses += 1
-        if response == 'h':
-            low = mid + 1
-        elif response == 'l':
-            high = mid - 1
-        else:
-            return guesses
+def number_guess():
+    global low, high, guesses
+    mid = (low + high) // 2
+    response  = input("Is your number " + str(mid) + "? ([h]igh/[l]ow/[e]qual)")
+    guesses += 1
+    if response == 'h':
+        low = mid + 1
+    elif response == 'l':
+        high = mid - 1
+    else:
+        return guesses
+    return False
 
 
 def display_output(guesses):
@@ -25,12 +30,12 @@ def display_output(guesses):
 
 
 def main():
-    low = 0
-    high = 100
-    guesses = 0
     ask_for_number()
-    guesses = process_guess(low, high, guesses)
-    display_output(guesses)
-    
-    
+    while True:
+        result = number_guess()
+        if result:
+            display_output(result)
+            break
+
+
 main()
