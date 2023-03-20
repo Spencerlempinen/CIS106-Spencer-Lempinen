089dd4fd6c2e245a7fdae45dcce97f72e84c3e2a
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-03-20 16:41:11 -0500

    Update Activity 1.py

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -10,41 +10,40 @@ days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 def get_year():
-    print("please enter a year: ")
+    print("Please enter a year: ")
     year = int(input())
     return year
 
 
-def month_num():
-    month = int(input("Please enter a month (1-12)"))
-    while month > 12:
-        month = int(input("Please enter a valid month number (1-12)"))
+def get_month():
+    print("Please enter a month (1-12): ")
+    month = int(input())
     return month
+    
 
 
 def calculate_days(year, month):
-    if month == 2 and (year % 4 == 0 and year % 100 != 0 or year% 400 == 0):
+    if month == 2 and (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
         days = 29
-        return days
     else:
         days = days_in_month[month - 1]
-        return days
+    return days
     
 
 def display_output(days, month, year):
-    print(f" {month_names [month-1]} {year} has {days} days")
+    print(f"{month_names[month - 1]} {year} has {days} days.")
 
 
 def main():
     while True:
         year = get_year()
-        month = month_num()
+        month = get_month()
         if month == 0:
-            print("0 is an invalid input")
-            break
-        else:
-            days = calculate_days(year, month)
-            display_output(days, month, year)
-       
+            print("0 is an invalid input.")
+            return
+        days = calculate_days(year, month)
+        display_output(days, month, year)
+        break
 
-main()
+    
+main()   
