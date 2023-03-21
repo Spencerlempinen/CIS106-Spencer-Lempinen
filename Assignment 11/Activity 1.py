d2d4343a728a32b2333680d002699c71e3a79a77
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-03-21 10:14:18 -0500

    Update Activity 1.py

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -3,8 +3,10 @@
 # https://www.youtube.com/watch?v=6a39OjkCN5I&ab_channel=Telusko
 
 
-month_names = ['January', 'February', 'March', 'April', 'May', 'June', 
-               'July', 'August', 'September', 'October', 'November', 'December']
+month_names = ['January', 'February', 'March', 
+               'April', 'May', 'June', 
+               'July', 'August', 'September', 
+               'October', 'November', 'December']
 
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -19,12 +21,11 @@ def get_month():
     while True:
         print("Please enter a month (1-12): ")
         month = int(input())
-        if month <1 or month >12:
+        if month < 1 or month > 12:
             print(f"{month} is an invalid input.") 
         else:
             return month
     
-
 
 def calculate_days(year, month):
     if month == 2 and (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
@@ -46,4 +47,3 @@ def main():
     
 
 main() 
-
