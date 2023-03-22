fc9d7be6adead8755bc04102468642d9e5918675
Dave Braunschweig <dave@itrain.ms>
2023-03-22 11:53:31 -0500

    Assignment corrections

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -3,14 +3,6 @@
 # https://www.youtube.com/watch?v=6a39OjkCN5I&ab_channel=Telusko
 
 
-month_names = ['January', 'February', 'March', 
-               'April', 'May', 'June', 
-               'July', 'August', 'September', 
-               'October', 'November', 'December']
-
-days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-
 def get_year():
     print("Please enter a year: ")
     year = int(input())
@@ -27,23 +19,47 @@ def get_month():
             return month
     
 
-def calculate_days(year, month):
-    if month == 2 and (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
-        days = 29
-    else:
-        days = days_in_month[month - 1]
-    return days
-    
+def get_month_name(month):
+    month_names = ['January', 'February', 'March', 
+        'April', 'May', 'June', 
+        'July', 'August', 'September', 
+        'October', 'November', 'December']
 
-def display_output(days, month, year):
-    print(f"{month_names[month - 1]} {year} has {days} days.")
+    if month < 1 or month > 12:
+        return None
+    
+    return month_names[month - 1]
+
+
+def get_month_days(year, month):
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if month < 1 or month > 12:
+        return None
+
+    if calculate_leap_year(year):
+        days_in_month[1] = 29
+    
+    return days_in_month[month - 1]
+
+
+def calculate_leap_year(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+def display_output(month, days, year):
+    print(f"{month} {year} has {days} days.")
 
 
 def main():
     year = get_year()
     month = get_month()
-    days = calculate_days(year, month)
-    display_output(days, month, year)
+    month_name = get_month_name(month)
+    month_days = get_month_days(year, month)
+    display_output(month_name, month_days, year)
     
 
 main() 
