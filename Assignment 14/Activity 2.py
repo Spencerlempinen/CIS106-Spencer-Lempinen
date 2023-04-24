34db2a427deacd6669fa352598b125ae78c7c05d
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-04-24 11:39:14 -0500

    Update Activity 2.py

--- Assignment 14/Activity 2.py
+++ Assignment 14/Activity 2.py
@@ -1,9 +1,9 @@
-# this program will average a set of scores from a .txt file
-# references:
+# This program will average a set of scores from a .txt file.
+# References: 
 # https://www.youtube.com/watch?v=XPO6ITK2Ecc&ab_channel=ProgrammingGuru
- 
 
- def read_scores(filename):
+
+def read_scores(filename):
     scores = []
     try:
         with open(filename, 'r') as f:
@@ -22,7 +22,7 @@
         print(f"Error: {filename} not found")
         return None
 
-
+      
 def parse_score(line):
     fields = line.strip().split(',')
     if len(fields) != 2:
@@ -34,25 +34,25 @@ def parse_score(line):
         return None
     return score
 
-
+   
 def calculate_high_score(scores):
     if not scores:
         return None
     return max(scores)
 
-
+   
 def calculate_low_score(scores):
     if not scores:
         return None
     return min(scores)
 
-
+   
 def calculate_average_score(scores):
     if not scores:
         return None
     return round(sum(scores) / len(scores), 2)
 
-
+   
 def display_scores(scores, high, low, average):
     print("Scores:")
     for score in scores:
@@ -61,7 +61,7 @@ def display_scores(scores, high, low, average):
     print(f"Lowest score: {low}")
     print(f"Average score: {average}")
 
-
+    
 def main():
     filename = "scores.txt"
     scores = read_scores(filename)
@@ -70,6 +70,8 @@ def main():
         low = calculate_low_score(scores)
         average = calculate_average_score(scores)
         display_scores(scores, high, low, average)
-
-
+        
+        
 main()
+
+
