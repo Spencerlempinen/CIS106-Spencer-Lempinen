47d637174ab46ca229f344dede2f74e897bf317b
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-04-24 11:20:38 -0500

    Update Activity 2.py

--- Assignment 14/Activity 2.py
+++ Assignment 14/Activity 2.py
@@ -3,64 +3,73 @@
 # https://www.youtube.com/watch?v=XPO6ITK2Ecc&ab_channel=ProgrammingGuru
  
 
-def read_scores(filename):
+ def read_scores(filename):
+    scores = []
     try:
         with open(filename, 'r') as f:
             lines = f.readlines()[1:]
             if not lines:
-                file_errors('empty_file')
+                print("Error: empty file")
                 return None
-            scores = process_scores(lines)
-            if scores is None:
-                file_errors('bad_data')
-                return None
+            for line in lines:
+                score = parse_score(line)
+                if score is None:
+                    print("Error: missing or bad data")
+                    return None
+                scores.append(score)
             return scores
     except FileNotFoundError:
-        file_errors('no_file', filename=filename)
+        print(f"Error: {filename} not found")
         return None
 
 
-def process_scores(lines):
-    scores = []
-    for line in lines:
-        fields = line.strip().split(',')
-        if len(fields) != 2:
-            return None
-        name, score = fields
-        try:
-            score = int(score)
-        except ValueError:
-            return None
-        scores.append(score)
-    return scores
+def parse_score(line):
+    fields = line.strip().split(',')
+    if len(fields) != 2:
+        return None
+    name, score = fields
+    try:
+        score = int(score)
+    except ValueError:
+        return None
+    return score
 
 
-def file_errors(error_type, filename=None):
-    if error_type == 'bad_data':
-        print("Error: missing or bad data.")
-    elif error_type == 'empty_file':
-        print("Error: file is empty.")
-    elif error_type == 'no_file':
-        print(f"Error: file {filename} not found.")
+def calculate_high_score(scores):
+    if not scores:
+        return None
+    return max(scores)
 
 
-def display_scores(scores):
-    if scores is None:
-        return
+def calculate_low_score(scores):
+    if not scores:
+        return None
+    return min(scores)
+
+
+def calculate_average_score(scores):
+    if not scores:
+        return None
+    return round(sum(scores) / len(scores), 2)
+
+
+def display_scores(scores, high, low, average):
     print("Scores:")
     for score in scores:
         print(score)
-    print("Highest score:", max(scores))
-    print("Lowest score:", min(scores))
-    avg_score = sum(scores) / len(scores)
-    print("Average score: {:.2f}".format(avg_score))
+    print(f"Highest score: {high}")
+    print(f"Lowest score: {low}")
+    print(f"Average score: {average}")
 
 
 def main():
     filename = "scores.txt"
     scores = read_scores(filename)
     if scores is not None:
-        display_scores(scores)
+        high = calculate_high_score(scores)
+        low = calculate_low_score(scores)
+        average = calculate_average_score(scores)
+        display_scores(scores, high, low, average)
 
 
 main()
