55178323329891bd19545cb0055f229df8f29790
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-05-02 11:57:34 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,29 +1,30 @@
 import re
 import requests
 
-
 def read_catalog(url):
     titles = []
 
-    response = requests.get
+    response = requests.get(url)
     if response.status_code != 200:
-        print(f"failed to retrive data from {url}")
+        print(f"failed to retrieve data from {url}")
         return titles
 
-    cd_regex = re.compile(r'<CD>(.*?)</CD>', re.DOTALL)
+    cd_regex = re.compile(r'<CD>(.+?)</CD>', re.DOTALL)
     cds = re.findall(cd_regex, response.text)
 
     for cd in cds:
-        title = re.find(r'<TITLE>(.*?)<TITLE>', cd).group(1)
+        title = re.search(r'<TITLE>(.+?)</TITLE>', cd).group(1)
         titles.append(title)
     return titles
 
 def display_output(titles):
-    for i in range(len(titles)):
-        print(f"titles[i]")
-
+    for title in titles:
+        print(title)
 
 def main():
     url = "https://www.w3schools.com/xml/cd_catalog.xml"
     titles = read_catalog(url)
     display_output(titles)
+
+main()
+
