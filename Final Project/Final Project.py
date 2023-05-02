11e8bc76c8794b1f4a7d289518b6bfa5562d89a8
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-05-02 12:55:34 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -4,6 +4,7 @@ import requests
 def read_catalog(url):
     titles = []
     artists = []
+    countries = []
 
     response = requests.get(url)
     if response.status_code != 200:
@@ -16,18 +17,20 @@ def read_catalog(url):
     for cd in cds:
         title = re.search(r'<TITLE>(.+?)</TITLE>', cd).group(1)
         artist = re.search(r'<ARTIST>(.+?)</ARTIST>', cd).group(1)
+        country = re.search(r'<COUNTRY>(.+?)</COUNTRY', cd).group(1)
         titles.append(title)
         artists.append(artist)
+        countries.append(country)
 
-    return titles, artists
+    return titles, artists, countries
 
-def display_output(titles, artists):
+def display_output(titles, artists, countries):
     for i in range(len(titles)):
-        print(titles[i] + " - " + artists[i])
+        print(titles[i] + " - " + artists[i] + " - " + countries[i])
 
 def main():
     url = "https://www.w3schools.com/xml/cd_catalog.xml"
-    titles, artists = read_catalog(url)
-    display_output(titles, artists)
+    titles, artists, countries = read_catalog(url)
+    display_output(titles, artists, countries)
 
 main()
