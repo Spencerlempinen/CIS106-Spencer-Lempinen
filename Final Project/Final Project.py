90e0fee2461fd8bd0564385c71f2379e842e302d
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-05-03 12:19:29 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -5,6 +5,8 @@ def read_catalog(url):
     titles = []
     artists = []
     countries = []
+    prices = []
+    years = []
 
     response = requests.get(url)
     if response.status_code != 200:
@@ -18,21 +20,23 @@ def read_catalog(url):
         title = re.search(r'<TITLE>(.+?)</TITLE>', cd).group(1)
         artist = re.search(r'<ARTIST>(.+?)</ARTIST>', cd).group(1)
         country = re.search(r'<COUNTRY>(.+?)</COUNTRY', cd).group(1)
-        prices = re.search(r'<PRICE>(.+?)</PRICE>', cd).group(1)
+        prices = float(re.search(r'<PRICE>(.+?)</PRICE>', cd).group(1))
+        years = int(re.search(r'<YEAR>(.+?)</YEAR>', cd).group(1))
         titles.append(title)
         artists.append(artist)
         countries.append(country)
         prices.append(price)
+        years.append(year)
 
-    return titles, artists, countries, prices
+    return titles, artists, countries, prices, years
 
-def display_output(titles, artists, countries, prices):
+def display_output(titles, artists, countries, prices, years):
     for i in range(len(titles)):
-        print(titles[i] + " - " + artists[i] + " - " + countries[i] + " - " + prices[i])
+        print(titles[i] + " - " + artists[i] + " - " + countries[i] + " - " + "$" + prices[i] + " - " + years[i])
 
 def main():
     url = "https://www.w3schools.com/xml/cd_catalog.xml"
-    titles, artists, countries = read_catalog(url)
-    display_output(titles, artists, countries, prices)
+    titles, artists, countries, prices, years = read_catalog(url)
+    display_output(titles, artists, countries, prices, years)
 
 main()
