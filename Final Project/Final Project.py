3e0ac6da891e639e01ddb48ae9ab418fda555313
Spencerlempinen <122818156+Spencerlempinen@users.noreply.github.com>
2023-05-04 14:21:37 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -4,19 +4,21 @@
 # Nick Rich(friend who does cloud security with CDW) :helped with the get_page_text function
 
 
-
 import urllib.request
 import re
+
 
 def get_page_text(url):
     with urllib.request.urlopen(url) as response:
         page_text = response.read().decode('utf-8')
     return page_text
 
+
 def get_tag_values(page_text, tag_name):
     tag_regex = re.compile(rf'<{tag_name}>(.*?)</{tag_name}>')
     tag_values = re.findall(tag_regex, page_text)
     return tag_values
+
 
 def get_cd_data(page_text):
     titles = get_tag_values(page_text, "TITLE")
@@ -25,6 +27,7 @@ def get_cd_data(page_text):
     prices = [float(p) for p in get_tag_values(page_text, "PRICE")]
     years = [int(y) for y in get_tag_values(page_text, "YEAR")]
     return titles, artists, countries, prices, years
+
 
 def calculate_avg(prices):
     num_items = len(prices)
@@ -35,6 +38,7 @@ def calculate_avg(prices):
         avg_price = total_price / num_items
         return print(f"{num_items} items - ${avg_price:.2f} average price")
 
+    
 def display_catalog(titles, artists, countries, prices, years):
     if len(titles) == 0:
         print("Error: missing or bad data")
@@ -44,6 +48,7 @@ def display_catalog(titles, artists, countries, prices, years):
         for i in range(len(titles)):
             print(f"{titles[i]} - {artists[i]} - {countries[i]} - ${prices[i]:.2f} - {years[i]}")
 
+            
 def main():
     catalog_url = "https://www.w3schools.com/xml/cd_catalog.xml"
     page_text = get_page_text(catalog_url)
